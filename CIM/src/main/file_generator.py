05c2261393681e2ad7df081d16973e44a59f3cfa
Hernan Finucci <hfinucci@itba.edu.ar>
2023-03-12 17:31:58 -0300

    static file generator

--- CIM/src/main/file_generator.py
+++ CIM/src/main/file_generator.py
@@ -1,9 +1,16 @@
 import random
+import sys
 
-static_file = open("./resources/input_static.txt", "r")
+N = int(sys.argv[1])
+L = int(sys.argv[2])
+R = float(sys.argv[3])
 
-N = int(static_file.readline())
-L = int(static_file.readline())
+static_file = open("generated_static.txt", "w")
+
+static_file.write(str(N) + "\n")
+static_file.write(str(L) + "\n")
+for i in range(N):
+    static_file.write(str(R) + "\t" + "1.0" + "\n")
 
 static_file.close()
 
